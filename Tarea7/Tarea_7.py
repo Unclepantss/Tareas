9709a983335fec0099f74f1c07a53186c7e7df5d
Antonio Castillo <antonio.castillo@alumnos.ucn.cl>
2020-06-03 16:10:30 -0400

    tarea 7 completa??

---
+++ Tarea7/Tarea_7.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  3 12:48:09 2020
+
+@author: Antonio
+"""
+
+entrada = open("secuencias.txt","r");
+valor = 0
+N = 1
+for linea in entrada: 
+    datos = linea.split(",")  
+    num = int(datos[0])
+    if num > valor:
+       valor = num
+    elif num < valor:
+        valor = num
+        N += 1
+print(f"las secuencias existentes en este archivo son {N}")
