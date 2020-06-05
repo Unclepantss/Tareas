56867bde1299f0efa90ba1962f93729ede736e55
Antonio Castillo <antonio.castillo@alumnos.ucn.cl>
2020-06-04 20:42:25 -0400

    prueba excel

---
+++ ejemplo2_prueba2018/Problema_3.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 17:11:36 2020
+
+@author: Antonio
+"""
+
+open ("cables.txt")
+
+if linea == 3:
+    if cable_1 == "rojo":
+        print("corte el segundo cable");
+    elif cable_3 == "blanco":
+        print("corte el ultimo cable");
+    elif cable_1 == "azul" == cable_2:
+        print ("corte el segundo cable azul");
+    else:
+        print("corte el ultimo cable");
+elif linea == 4:
+    if "rojo" > 1:
+        print("corte el ultimo cable")
+    elif cable_4 == "amarillo" and 0 < "rojo":
+        print("corte el primer cable")
+    elif "rojo" == 1:
+        print ("corte el segundo cable")
+    elif cable_2 == cable_3 == "amarillo":
+        print("corte el ultimo cable")
+    else:
+        print("corte el ultimo cable")   
+elif linea == 5:
+    if cable == "negro":
+        print("corte el cuarto cable")
+    elif cable_3 == "rojo":
+        print("corte el tercer cable")
+    elif "negro" > 1:
+        print("corte el segundo cable")
+    else:
+        print("corte el primer cable")
